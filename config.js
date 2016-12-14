693822b93a2602afd5ca683054c6cdaa75d34969
DavidDaugela <daviddaugela@gmail.com>
2016-12-14 00:22:05 -0500

    INIT - FULL

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by DavidDaugela on 2016-12-13.
+ */
+
+
+// Twitter APP CONFIGURATION
+// consumer_key
+// consumer_secret
+// access_token
+// access_token_secret
+
+module.exports = {
+    consumer_key: 'uVhVNNlezO4fXiQcOsgGJLSR3',
+    consumer_secret: 'blhlAStVGVakjOEFMrom75ee1GBvlFMXP4xVwKLOcL6baECULS',
+    access_token: '787293926675865600-iekMn7F7yn8TnnWsYSyjnoJTINuPLo9',
+    access_token_secret: 'zSmrESasyxvZdRJnkQ7idVMYmgbOkVP0weT2xQZUK8ogw'
+
+}
